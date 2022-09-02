69208c19ad61dfc3c62d6d8e044f337c55936b70
jefersonfranco <jefersonfranco@protonmail.com>
2022-09-02 16:46:39 -0300

    update depositProfit.js

--- src/depositProfit.js
+++ src/depositProfit.js
@@ -1,7 +1,7 @@
 function solution(deposit, rate, threshold) {
     let years = 0;
     while (deposit < threshold) {
-        deposit += deposit * rate / 100;
+        deposit += (deposit * rate) / 100;
         years++;
     }
     return years;
@@ -13,3 +13,23 @@ module.exports = solution;
 // npm i jest-cli -g
 // package.json: "scripts": { "test": "jest" }
 // npm test depositProfit
+
+// alternative solution
+// function solution(d, r, t) {
+//     return Math.ceil(Math.log(t / d) / Math.log(r / 100 + 1));
+// }
+
+// alternative solution
+// function solution(deposit, rate, threshold) {
+//     let years = 0;
+//     while (deposit < threshold) {
+//         deposit *= 1 + rate / 100;
+//         years++;
+//     }
+//     return years;
+// }
+
+// alternative solution
+// function solution(deposit, rate, threshold) {
+//     return Math.ceil(Math.log(threshold / deposit) / Math.log(1 + rate / 100));
+// }
